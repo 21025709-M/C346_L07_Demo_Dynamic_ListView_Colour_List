df649cec0c3a25800dc3a3209303a5f417bb6787
21025709-M <21025709@myrp.edu.sg>
2023-06-06 12:58:29 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21025709/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.id21025709.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    Button btnAdd;
+    ListView lvColor;
+    ArrayList<String> alColours;
+    ArrayAdapter<String> aaColour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColor);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        lvColor = findViewById(R.id.listViewColour);
+
+        alColours = new ArrayList<>();
+        alColours.add("Red");
+        alColours.add("Orange");
+
+        aaColour = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alColours);
+        lvColor.setAdapter(aaColour);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString(); //extraction
+                alColours.add(colour);                          //insertion
+                aaColour.notifyDataSetChanged();                //Listview refresh
+            }
+        });
+    }
+}
